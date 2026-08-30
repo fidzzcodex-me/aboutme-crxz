14bafcd43f34771e0dcdd578e9b878631fbcf8be
fidzzcodex.me <rinatonidi28@gmail.com>
2026-08-30 18:43:34 +0700

    Upload: add data/stack.js

---
+++ data/stack.js
@@ -0,0 +1,14 @@
+export const stack = [
+  { name: "JavaScript", status: "used" },
+  { name: "React", status: "used" },
+  { name: "Node.js", status: "used" },
+  { name: "REST API", status: "used" },
+  { name: "HTML", status: "used" },
+  { name: "CSS", status: "used" },
+  { name: "Git", status: "used" },
+  { name: "GitHub", status: "used" },
+  { name: "Vercel", status: "used" },
+  { name: "VPS", status: "used" },
+  { name: "Pterodactyl", status: "used" },
+  { name: "Cloudflare", status: "learning" },
+];
